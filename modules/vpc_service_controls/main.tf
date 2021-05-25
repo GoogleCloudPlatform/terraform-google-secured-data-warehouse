575ae9fc539c0509137fe17b1df9265a03e0741a
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-05-25 09:59:15 -0300

    Adds vpc service control module

---
+++ modules/vpc_service_controls/main.tf
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  suffix                         = var.commom_suffix != "" ? var.commom_suffix : random_id.suffix.hex
+  actual_policy                  = var.access_context_manager_policy_id != "" ? var.access_context_manager_policy_id : google_access_context_manager_access_policy.access_policy[0].name
+  perimeter_name                 = "regular_perimeter_data_warehouse_${local.suffix}"
+  regular_service_perimeter_name = "accessPolicies/${local.actual_policy}/servicePerimeters/${local.perimeter_name}"
+  access_policy_name             = "enterprise_data_warehouse_policy_${local.suffix}"
+}
+
+resource "google_access_context_manager_access_policy" "access_policy" {
+  count  = var.access_context_manager_policy_id != "" ? 0 : 1
+  parent = "organizations/${var.org_id}"
+  title  = "default policy"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+data "google_project" "target_project" {
+  project_id = var.project_id
+}
+
+module "access_level_policy" {
+  # source = "terraform-google-modules/vpc-service-controls/google//modules/access_level"
+  # replace after new release from https://github.com/terraform-google-modules/terraform-google-vpc-service-controls
+  source = "github.com/terraform-google-modules/terraform-google-vpc-service-controls//modules/access_level"
+
+  policy      = local.actual_policy
+  name        = local.access_policy_name
+  description = "policy with all available options to configure"
+
+  members = distinct(concat([
+    "serviceAccount:${var.terraform_service_account}",
+  ], var.perimeter_additional_members))
+
+  ip_subnetworks = var.access_level_ip_subnetworks
+  regions        = var.access_level_regions
+
+  require_screen_lock         = var.organization_has_mdm_license ? var.access_level_require_screen_lock : false
+  allowed_encryption_statuses = var.organization_has_mdm_license ? var.access_level_allowed_encryption_statuses : []
+  require_corp_owned          = var.organization_has_mdm_license ? var.access_level_require_corp_owned : false
+}
+
+module "regular_service_perimeter" {
+  source = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
+
+  policy         = local.actual_policy
+  perimeter_name = local.perimeter_name
+  description    = "perimeter for data warehouse projects"
+
+  resources           = [data.google_project.target_project.number]
+  restricted_services = var.restricted_services
+
+  access_levels = [
+    module.access_level_policy.name
+  ]
+}
---
+++ modules/vpc_service_controls/network.tf
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  network_name               = "vpc-${var.vpc_name}"
+  restricted_googleapis_cidr = "199.36.153.4/30"
+  private_googleapis_cidr    = "199.36.153.8/30"
+}
+
+module "network" {
+  source                                 = "terraform-google-modules/network/google"
+  version                                = "~> 3.2"
+  project_id                             = var.project_id
+  network_name                           = local.network_name
+  shared_vpc_host                        = "false"
+  delete_default_internet_gateway_routes = "true"
+
+  subnets = [
+    {
+      subnet_name           = "sb-restricted-${var.region}"
+      subnet_ip             = var.subnet_ip
+      subnet_region         = var.region
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
+      description           = "restricted subnet."
+    }
+  ]
+
+  routes = [{
+    name              = "rt-${var.vpc_name}-1000-all-default-restricted-api"
+    description       = "Route through IGW to allow restricted google api access."
+    destination_range = local.restricted_googleapis_cidr
+    next_hop_internet = "true"
+    priority          = "1000"
+    },
+    {
+      name              = "rt-${var.vpc_name}-1000-all-default-private-api"
+      description       = "Route through IGW to allow private google api access."
+      destination_range = local.private_googleapis_cidr
+      next_hop_internet = "true"
+      priority          = "1000"
+  }]
+}
