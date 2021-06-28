26e757e110d13f3af031c693f77ded85e9a1a228
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-06-28 16:40:07 -0300

    Adds dataflow job check

--- test/fixtures/dataflow-with-dlp/outputs.tf
+++ test/fixtures/dataflow-with-dlp/outputs.tf
@@ -40,17 +40,17 @@ output "df_job_controller_service_account" {
   value       = data.google_service_account.dataflow_service_account.email
 }
 
+output "df_job_region" {
+  description = "The name of the network used for create Dataflow job"
+  value       = module.dataflow-with-dlp.df_job_region
+}
+
 output "df_job_network" {
   description = "The name of the network used for create Dataflow job"
   value       = data.google_compute_network.vpc_network.id
 }
 
 output "df_job_subnetwork" {
-  description = "The name of the network used for create Dataflow job"
-  value       = data.google_compute_network.vpc_network.subnetworks_self_links[0]
-}
-
-output "df_job_subnetwork_region" {
   description = "The name of the network used for create Dataflow job"
   value       = data.google_compute_network.vpc_network.subnetworks_self_links[0]
 }
