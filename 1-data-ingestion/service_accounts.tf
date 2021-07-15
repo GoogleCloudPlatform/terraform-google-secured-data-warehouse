0a07527109793822a75d4b13e844926b618787ad
Ann Wallace <anners@google.com>
2021-07-15 08:09:42 -0700

    fixed linting issues

--- 1-data-ingestion/service_accounts.tf
+++ 1-data-ingestion/service_accounts.tf
@@ -20,7 +20,7 @@ module "dataflow_controller_service_account" {
   source       = "terraform-google-modules/service-accounts/google"
   version      = "~> 3.0"
   project_id   = var.project_id
-  account_id   = ["sa-dataflow-controller"]
+  names        = ["sa-dataflow-controller"]
   display_name = "Cloud Dataflow controller service account"
   project_roles = [
     "${var.project_id}=>roles/pubsub.subscriber",
