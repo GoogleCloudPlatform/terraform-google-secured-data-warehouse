f2cedb3343b5f9eade8ec0f356435c1dc5664735
Luiz Dutra <luizdutra@ciandt.com>
2021-08-30 09:06:31 -0300

    fixed outputs and variable

--- examples/dataflow-with-dlp/variables.tf
+++ examples/dataflow-with-dlp/variables.tf
@@ -40,13 +40,13 @@ variable "project_id" {
 }
 
 variable "access_context_manager_policy_id" {
-  type        = number
   description = "The id of the default Access Context Manager policy. Can be obtained by running `gcloud access-context-manager policies list --organization YOUR-ORGANIZATION_ID --format=\"value(name)\"`."
+  type        = number
 }
 
 variable "bucket_force_destroy" {
-  type        = bool
   description = "When deleting a bucket, this boolean option will delete all contained objects. If you try to delete a bucket that contains objects, Terraform will fail that run."
+  type        = bool
   default     = false
 }
 
