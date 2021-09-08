817c1b02a39985daaca5f78fe5bc47976f0b6913
renato-rudnicki <77694243+renato-rudnicki@users.noreply.github.com>
2021-09-08 15:12:17 -0300

    Add org_policies to main module (#125)
    * moving org_policies module to main module
    
    * removing standalone fixture/tests
    
    * changing type value for trusted_subnetwork
    
    * adding org_policies test in simple_example
    
    * adding simple_example as first in cloudbuild for testing purpose
    
    * changing var.trusted_subnetwork to string
    
    * changing value for trusted_subnetwork variable
    
    * fix lint
    
    * updating propagation configuration

--- main.tf
+++ main.tf
@@ -43,3 +43,11 @@ module "data_ingestion" {
   cmek_keyring_name                   = var.cmek_keyring_name
   subnet_ip                           = var.subnet_ip
 }
+
+module "org_policies" {
+  source             = "./modules/org_policies"
+  project_id         = var.project_id
+  region             = local.region
+  trusted_subnetwork = module.data_ingestion.subnets_names[0]
+  trusted_locations  = var.trusted_locations
+}
