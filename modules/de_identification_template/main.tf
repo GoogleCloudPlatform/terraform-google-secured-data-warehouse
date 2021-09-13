1595eefac4422658d9e3383da2e4743d19613034
Daniel da Silva Andrade <dandrade@ciandt.com>
2021-09-12 22:17:52 -0300

    remove google terraform gcloud module

--- modules/de_identification_template/main.tf
+++ modules/de_identification_template/main.tf
@@ -50,15 +50,20 @@ resource "google_kms_crypto_key_iam_binding" "dlp_encrypters_decrypters" {
   members       = ["serviceAccount:${var.dataflow_service_account}"]
 }
 
-module "de_identify_template" {
-  source  = "terraform-google-modules/gcloud/google"
-  version = "~> 3.0"
+resource "null_resource" "de_identify_template" {
 
-  skip_download = true
+  triggers = {
+    template                  = local.de_identification_template,
+    project_id                = var.project_id,
+    template_id               = local.template_id
+    dlp_location              = var.dlp_location
+    terraform_service_account = var.terraform_service_account
+  }
 
-  create_cmd_entrypoint = "curl"
-  create_cmd_body       = <<EOF
-    -s https://dlp.googleapis.com/v2/projects/${var.project_id}/locations/${var.dlp_location}/deidentifyTemplates \
+  provisioner "local-exec" {
+    when    = create
+    command = <<EOF
+    curl -s https://dlp.googleapis.com/v2/projects/${var.project_id}/locations/${var.dlp_location}/deidentifyTemplates \
     --header "X-Goog-User-Project: ${var.project_id}" \
     --header "Authorization: Bearer $(gcloud auth print-access-token --impersonate-service-account=${var.terraform_service_account})" \
     --header 'Accept: application/json' \
@@ -66,14 +71,18 @@ module "de_identify_template" {
     --data '${local.de_identification_template}'
 EOF
 
-  destroy_cmd_entrypoint = "curl"
-  destroy_cmd_body       = <<EOF
-    -s --request DELETE \
-    https://dlp.googleapis.com/v2/projects/${var.project_id}/locations/${var.dlp_location}/deidentifyTemplates/${local.template_id} \
-    --header "X-Goog-User-Project: ${var.project_id}" \
-    --header "Authorization: Bearer $(gcloud auth print-access-token --impersonate-service-account=${var.terraform_service_account})" \
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOF
+    curl -s --request DELETE \
+    https://dlp.googleapis.com/v2/projects/${self.triggers.project_id}/locations/${self.triggers.dlp_location}/deidentifyTemplates/${self.triggers.template_id} \
+    --header "X-Goog-User-Project: ${self.triggers.project_id}" \
+    --header "Authorization: Bearer $(gcloud auth print-access-token --impersonate-service-account=${self.triggers.terraform_service_account})" \
     --header 'Accept: application/json' \
     --header "Content-Type: application/json"
 EOF
 
+  }
 }
