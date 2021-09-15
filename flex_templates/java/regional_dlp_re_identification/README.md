4b5a39030764a88bf00ad79c246aacf8095d7d13
erlander <erlander@google.com>
2021-09-15 09:36:03 -0500

    reidentification dataflow
        - structure lots of flow from reid solution
    
        - fix packages and classpath
    
        - clean compile first
    
        - fix metadata input for flex template
    
        - pipeline working
    
        - cleanup unused imports and deadcode

---
+++ flex_templates/java/regional_dlp_re_identification/README.md
@@ -0,0 +1,73 @@
+# Regional DLP re-identification BigQuery to BigQuery flex template
+
+## Build the flex template with Cloud Build
+
+Set the following environment variables based in the resources create in the infrastructure step:
+
+```shell
+export LOCATION=<REPOSITORY-LOCATION>
+export PROJECT=<YOUR-PROJECT>
+export BUCKET=<YOUR-FLEX-TEMPLATE-BUCKET>
+export FLEX_REPO_URL=$LOCATION-docker.pkg.dev/$PROJECT/flex-templates
+```
+
+```shell
+# build the flex template
+
+gcloud beta builds submit \
+ --project=$PROJECT \
+ --config ./cloudbuild.yaml . \
+ --substitutions="_BUCKET=$BUCKET,_PROJECT=$PROJECT,_FLEX_REPO_URL=$FLEX_REPO_URL"
+ ```
+
+**Note:** It is possible to migrate the maven image used to a Artifact Registry in your organization.
+Follow the instructions in [Migrating containers from a third-party registry](https://cloud.google.com/artifact-registry/docs/docker/migrate-external-containers)
+to migrate the `maven:3.8.2-jdk-11` image.
+
+After migrating the image, update the cloudbuild.yaml file to used the image in the new repository, for example, for the artifact registry repository with name `migrated-images` in the `us-central1` location at project `your-project` use:
+
+```diff
+steps:
+- - name: 'maven:3.8.2-jdk-11'
++ - name: 'us-central1-docker.pkg.dev/your-project/migrated-images/maven:3.8.2-jdk-11'
+  entrypoint: 'mvn'
+```
+
+## Run the flex template manually
+
+1.  Follow the instructions in [Using Flex Templates:Setting up your development environment](https://cloud.google.com/dataflow/docs/guides/templates/using-flex-templates#setting_up_your_development_environment) to configure your environment to build the images.
+2.  Build the flex template
+3.  make sure you have input BigQuery table avaialable with deidentified data (and corresponding DLP template)
+4.  apply the `example/bigquery_sensitive_data`
+5.  run the dataflow job
+
+```shell
+export LOCATION=<REPOSITORY-LOCATION>
+export PROJECT=<YOUR-PROJECT>
+export TEMPLATE_PATH=<YOUR_GCS_PATH> # e.g "gs://BUCKET/flex-template-samples/regional-bq-dlp-bq-streaming.json"
+```
+
+Run the Flex Image:
+
+```shell
+gcloud dataflow flex-template run "regional-bq-dlp-bq-`date +%Y%m%d-%H%M%S`" \
+    --template-file-gcs-location "$TEMPLATE_PATH" \
+    --parameters inputBigQueryTable="PROJECT:DATASET.TABLE" \
+    --parameters outputBigQueryDataset="DATASET_NAME" \
+    --parameters deidentifyTemplateName="FULL_DEIDENTIFY_TEMPLATE_NAME" \
+    --parameters dlpProjectId="DLP_PROJECT_ID" \
+    --parameters dlpLocation="DLP_LOCATION" \
+    --parameters privilegedDataProjectId="PRIVILEGED_DATA_PROJECT_ID" \
+    --project=${PROJECT} \
+    --service-account-email="DATAFLOW_SERVICE_ACCOUNT" \
+    --network="NETWORK" \
+    --subnetwork="SUBNET" \
+    --region="${LOCATION}" \
+    --disable-public-ips \
+    --enable-streaming-engine
+```
+
+## TODO
+- [ ] clean up POM file
+- [ ] add logging back in
+- [ ] fix null ptr exception with inspectionTemplates (and add that back in)
---
+++ flex_templates/java/regional_dlp_re_identification/src/main/java/com/google/cloud/blueprints/datawarehouse/BigQueryDLPBigQuery.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.blueprints.datawarehouse;
+
+import com.google.api.services.bigquery.model.TableRow;
+import java.util.List;
+import org.apache.beam.runners.dataflow.options.DataflowWorkerLoggingOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.GroupByKey;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.View;
+import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindows;
+import org.apache.beam.sdk.transforms.windowing.Repeatedly;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link BigQueryDLPBigQuery} is a streaming pipeline that reads BigQuery
+ * tables, uses Cloud DLP API to inspect and classify sensitive information (e.g. PII Data like
+ * passport or SIN number) and at the end stores tags in Data Catalog to be used for various
+ * purposes.
+ */
+public class BigQueryDLPBigQuery {
+  private static final String PIPELINE_PACKAGE_NAME = "com.google.cloud.blueprints.datawarehouse";
+  public static final Logger LOG = LoggerFactory.getLogger(BigQueryDLPBigQuery.class);
+
+  /**
+   * Main entry point for executing the pipeline. This will run the pipeline asynchronously. If
+   * blocking execution is required, use the {@link
+   * DLP2DatacatalogTagsAllBigQueryInspection method to start the
+   * pipeline and invoke {@code result.waitUntilFinish()} on the {@link PipelineResult}
+   *
+   * @param args The command-line arguments to the pipeline.
+   */
+  public static void main(String[] args) {
+
+    // @TODO re-add try-catch later
+    // try (?){
+    BigQueryReidentifyPipelineOptions options =
+          PipelineOptionsFactory.fromArgs(args)
+              .as(BigQueryReidentifyPipelineOptions.class);
+
+    //TODO figure out logging
+    //Boolean verboseLogging = getPropertiesBool(prop, "verbose.logging", "false");
+    //   Boolean verboseLogging = true;
+    //   if (verboseLogging) {
+    //     DataflowWorkerLoggingOptions loggingOptions =
+    //         options.as(DataflowWorkerLoggingOptions.class);
+
+    //     // Overrides logger for package.
+    //     DataflowWorkerLoggingOptions.WorkerLogLevelOverrides workerLogger =
+    //         new DataflowWorkerLoggingOptions.WorkerLogLevelOverrides();
+
+    //     // Set debug level for the pipeline class
+    //     loggingOptions.setWorkerLogLevelOverrides(
+    //         workerLogger.addOverrideForPackage(
+    //             Package.getPackage(PIPELINE_PACKAGE_NAME),
+    //             DataflowWorkerLoggingOptions.Level.DEBUG));
+    //   }
+
+      run(options);
+
+    // @TODO add back try-catch
+    // } catch (IOException ex) {
+    //   ex.printStackTrace();
+    // }
+  }
+
+
+  /**
+   * Runs the pipeline with the supplied options.
+   * https://github.com/GoogleCloudPlatform/dlp-dataflow-deidentification/blob/2375b8af9017a32a836ae09a43b3e498a5db63f1/src/main/java/com/google/swarm/tokenization/DLPTextToBigQueryStreamingV2.java#L204
+   *
+   * @param options The execution parameters to the pipeline.
+   * @return The result of the pipeline execution.
+   */
+  public static PipelineResult run(BigQueryReidentifyPipelineOptions options) {
+    // Create the pipeline
+    Pipeline p = Pipeline.create(options);
+
+    /*
+    *  Steps:
+    *    1) Read record from input BQ table
+    *    2) get header
+    *    3) convert to DLP
+    *    4) re-identify with DLP
+    *    5) convert to BQ tablerow
+    *    6) write re-identity data to output BQ table
+    */
+
+    PCollection<KV<String, TableRow>> record =
+    p.apply(
+        "ReadFromBQ",
+        BigQueryReadTransform.newBuilder()
+            .setTableRef(options.getInputBigQueryTable())
+            .build());
+
+    PCollectionView<List<String>> selectedColumns =
+        record
+            .apply(
+                "GlobalWindow",
+                Window.<KV<String, TableRow>>into(new GlobalWindows())
+                    .triggering(
+                        Repeatedly.forever(AfterProcessingTime.pastFirstElementInPane()))
+                    .discardingFiredPanes())
+            .apply(
+                "GroupByTableName",
+                GroupByKey.create())
+            .apply(
+                "GetHeader",
+                ParDo.of(new BigQueryTableHeaderDoFn())
+            )
+            .apply(
+                "ViewAsList",
+                View.asList()
+            );
+
+    PCollection<KV<String, TableRow>> reidData =
+        record
+            .apply("ConvertTableRow", ParDo.of(new MergeBigQueryRowToDlpRow()))
+            .apply( "DLPTransform",
+            DLPTransform.newBuilder()
+                .setBatchSize(options.getBatchSize())
+                .setDeidTemplateName(options.getDeidentifyTemplateName())
+                .setProjectId(options.getDlpProjectId())
+                .setDlpLocation(options.getDlpLocation())
+                .setHeader(selectedColumns)
+                .setColumnDelimiter(options.getColumnDelimiter())
+                .build())
+        .get(Util.reidSuccess);
+
+    // BQ insert
+    reidData.apply(
+        "BigQueryInsert",
+        BigQueryDynamicWriteTransform.newBuilder()
+            .setDatasetId(options.getOutputBigQueryDataset())
+            .setProjectId(options.getProject())
+            .build());
+
+      return p.run();
+  }
+
+}
