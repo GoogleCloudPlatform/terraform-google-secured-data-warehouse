41a553850bea109b3516f877c955d4896ce6dc68
Daniel da Silva Andrade <dandrade@ciandt.com>
2021-09-14 22:39:47 -0300

    fix project where to creaate the DLP template

--- examples/dataflow-with-dlp/main.tf
+++ examples/dataflow-with-dlp/main.tf
@@ -90,7 +90,7 @@ EOF
 module "de_identification_template" {
   source = "../..//modules/de_identification_template"
 
-  project_id                = var.data_ingestion_project_id
+  project_id                = var.data_governance_project_id
   terraform_service_account = var.terraform_service_account
   crypto_key                = var.crypto_key
   wrapped_key               = var.wrapped_key
@@ -127,7 +127,7 @@ module "dataflow_job" {
     datasetName            = local.dataset_id
     batchSize              = 1000
     dlpProjectId           = var.data_ingestion_project_id
-    deidentifyTemplateName = "projects/${var.data_ingestion_project_id}/locations/${local.region}/deidentifyTemplates/${module.de_identification_template.template_id}"
+    deidentifyTemplateName = "projects/${var.data_governance_project_id}/locations/${local.region}/deidentifyTemplates/${module.de_identification_template.template_id}"
   }
 
   depends_on = [
