7eab568bb04fd2bc18868086ba06922cfec866eb
Daniel da Silva Andrade <dandrade@ciandt.com>
2021-09-29 18:33:12 -0300

    Fix ruby extra 'end' error

--- test/integration/bigquery-sensitive-data/controls/gcloud.rb
+++ test/integration/bigquery-sensitive-data/controls/gcloud.rb
@@ -18,48 +18,21 @@ taxonomy_name              = attribute('taxonomy_name')
 control 'gcloud' do
   title 'Gcloud Resources'
 
-    describe command("gcloud data-catalog taxonomies list --location='us-east4' --project=#{data_governance_project_id}  --filter=displayName=#{taxonomy_name} --format=json") do
-      its(:exit_status) { should eq 0 }
+  describe command("gcloud data-catalog taxonomies list --location='us-east4' --project=#{data_governance_project_id}  --filter=displayName=#{taxonomy_name} --format=json") do
+    its(:exit_status) { should eq 0 }
 
-      let(:data) do
-        if subject.exit_status.zero?
-          JSON.parse(subject.stdout)
-        else
-          {}
-        end
+    let(:data) do
+      if subject.exit_status.zero?
+        JSON.parse(subject.stdout)
+      else
+        {}
       end
-
-      describe "Taxonomy #{taxonomy_name}" do
-        it 'should exist' do
-          expect(data[0]).to_not be_empty
-        end
-      end
-
     end
 
-  # # The test below depends of the fix from the bug https://github.com/GoogleCloudPlatform/terraform-google-secured-data-warehouse/issues/35
-  #
-  #   describe command("bq show --schema  --headless --location='us-east4' --project_id=#{data_governance_project_id} dtwh_dataset.sample_data") do
-  #     its(:exit_status) { should eq 0 }
-  #     its(:stderr) { should eq '' }
-  #     let(:data) do
-  #       if subject.exit_status.zero?
-  #         JSON.parse(subject.stdout)
-  #       else
-  #         {}
-  #       end
-  #     end
-  #     it { expect(data).to include(
-  #       including(
-  #         'name' => 'social_security_number', 'policyTags' => including('names' => including("#{social_security_number_policy_tag}")))
-  #       )
-  #     }
-  #     it { expect(data).to include(
-  #       including(
-  #         'name' => 'name', 'policyTags' => including('names' => including("#{person_name_policy_tag}")))
-  #       )
-  #     }
-  #   end
+    describe "Taxonomy #{taxonomy_name}" do
+      it 'should exist' do
+        expect(data[0]).to_not be_empty
+      end
+    end
   end
 end
-
