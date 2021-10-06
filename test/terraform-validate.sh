85097f00454144afe6fbba86cca383100e180c1a
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-10-06 15:02:04 -0300

    Adds export setup outputs

--- test/terraform-validate.sh
+++ test/terraform-validate.sh
@@ -63,15 +63,15 @@ else
 
         cd "$path" || exit
 
-        export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=${TF_VAR_terraform_service_account}
-
         terraform init -upgrade
         terraform plan -input=false -out "${tmp_plan}/${tf_example}.tfplan"  || exit 31
         terraform show -json "${tmp_plan}/${tf_example}.tfplan" > "${tf_example}.json" || exit 32
+
+        export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=${TF_VAR_terraform_service_account}
         terraform-validator validate "${tf_example}.json" --policy-path="${policy_file_path}" --project="${PROJECT_ID}" || exit 33
+        unset GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
 
         cd "$base_dir" || exit
-        unset GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
     else
       echo "ERROR:  ${path} does not exist"
     fi
