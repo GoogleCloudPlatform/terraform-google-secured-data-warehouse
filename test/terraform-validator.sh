b26f20caba85d457f88cd856e4333c4383a6976b
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-10-22 09:39:59 -0300

    Removes export from script

--- test/terraform-validator.sh
+++ test/terraform-validator.sh
@@ -19,8 +19,8 @@ set -e
 tf_example=$1
 
 IFS=',' read -ra projects <<< "$TF_VAR_data_ingestion_project_id"
-export project=$(echo ${projects[0]} | tr -d \" | tr -d \[ | tr -d \])
-export policy_file_path="$(pwd)/policy-library"
-export fixtures_path="test/fixtures"
+project=$(echo ${projects[0]} | tr -d \" | tr -d \[ | tr -d \])
+policy_file_path="$(pwd)/policy-library"
+fixtures_path="test/fixtures"
 
 source /usr/local/bin/test_validator.sh "${fixtures_path}/${tf_example}" "${project}" "${policy_file_path}"
