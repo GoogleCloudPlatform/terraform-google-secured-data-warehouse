093277ef1ca93547da86373b7a434140f4feaf96
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-11-25 09:29:54 -0300

    Fix lint issues

--- helpers/wrapped_key.py
+++ helpers/wrapped_key.py
@@ -4,10 +4,10 @@ def encrypt_symmetric(project_id, location_id, key_ring_id, key_id, plaintext):
     Encrypt plaintext using a symmetric key.
 
     Args:
-        project_id (string): Google Cloud project ID (e.g. 'my-project').
-        location_id (string): Cloud KMS location (e.g. 'us-east1').
-        key_ring_id (string): ID of the Cloud KMS key ring (e.g. 'my-key-ring').
-        key_id (string): ID of the key to use (e.g. 'my-key').
+        project_id (string): Google Cloud project ID.
+        location_id (string): Cloud KMS location.
+        key_ring_id (string): ID of the Cloud KMS key ring.
+        key_id (string): ID of the key to use.
         plaintext (string): message to encrypt
 
     Returns:
@@ -31,23 +31,30 @@ def encrypt_symmetric(project_id, location_id, key_ring_id, key_id, plaintext):
     client = kms.KeyManagementServiceClient()
 
     # Build the key name.
-    key_name = client.crypto_key_path(project_id, location_id, key_ring_id, key_id)
+    key_name = client.crypto_key_path(
+        project_id, location_id, key_ring_id, key_id)
 
     # Call the API.
     encrypt_response = client.encrypt(
-      request={'name': key_name, 'plaintext': plaintext_bytes, 'plaintext_crc32c': plaintext_crc32c})
+        request={'name': key_name, 'plaintext': plaintext_bytes,
+                 'plaintext_crc32c': plaintext_crc32c})
 
-    # Optional, but recommended: perform integrity verification on encrypt_response.
-    # For more details on ensuring E2E in-transit integrity to and from Cloud KMS visit:
+    # Optional, but recommended: perform integrity verification
+    # on encrypt_response.
+    # For more details on ensuring E2E in-transit integrity to
+    # and from Cloud KMS visit:
     # https://cloud.google.com/kms/docs/data-integrity-guidelines
     if not encrypt_response.verified_plaintext_crc32c:
-        raise Exception('The request sent to the server was corrupted in-transit.')
+        raise Exception(
+            'The request sent to the server was corrupted in-transit.')
     if not encrypt_response.ciphertext_crc32c == crc32c(encrypt_response.ciphertext):
-        raise Exception('The response received from the server was corrupted in-transit.')
+        raise Exception(
+            'The response received from the server was corrupted in-transit.')
     # End integrity verification
 
     print('Ciphertext: {}'.format(base64.b64encode(encrypt_response.ciphertext)))
     return encrypt_response
+
 
 def crc32c(data):
     """
@@ -66,19 +73,22 @@ def crc32c(data):
 
 
 if __name__ == '__main__':
-    import argparse, sys
+    import argparse
+    import sys
 
-    parser = argparse.ArgumentParser(description='Encrypt plaintext using a symmetric key.')
+    parser = argparse.ArgumentParser(
+        description='Encrypt plaintext using a symmetric key.')
     parser.add_argument('--project_id', dest='project_id',
-                        help='project_id (string): Google Cloud project ID (e.g. "my-project").')
+                        help='project_id (string): Google Cloud project ID.')
     parser.add_argument('--location_id', dest='location_id',
-                        help='location_id (string): Cloud KMS location (e.g. "us-east1").')
+                        help='location_id (string): Cloud KMS location.')
     parser.add_argument('--key_ring_id', dest='key_ring_id',
-                        help='key_ring_id (string): ID of the Cloud KMS key ring (e.g. "my-key-ring").')
+                        help='key_ring_id (string): ID of the Cloud KMS key ring.')
     parser.add_argument('--key_id', dest='key_id',
-                        help='key_id (string): ID of the key to use (e.g. "my-key").')
+                        help='key_id (string): ID of the key to use.')
     parser.add_argument('--plaintext', dest='plaintext',
                         help='plaintext (string): message to encrypt')
 
     args = parser.parse_args()
-    sys.exit(encrypt_symmetric(args.project_id, args.location_id, args.key_ring_id, args.key_id, args.plaintext))
+    sys.exit(encrypt_symmetric(args.project_id, args.location_id,
+             args.key_ring_id, args.key_id, args.plaintext))
