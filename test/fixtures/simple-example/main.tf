8ad704071ea032e8609ac45b71e179b34059381f
Daniel Andrade <dandrade@ciandt.com>
2021-12-16 00:55:00 -0300

    chore: remove test infrastructure for Java examples (#240)
    * remove test infrastructure for Java examples
    
    * fix de-identification-template output
    
    * update policy library policy constraints to remove extra project id
    
    * remove third network from policy library policy constraints
    
    * remove extra project number from policy library policy constraint

--- test/fixtures/simple-example/main.tf
+++ test/fixtures/simple-example/main.tf
@@ -18,18 +18,18 @@
 module "simple_example" {
   source                           = "../../..//examples/simple-example"
   org_id                           = var.org_id
-  data_governance_project_id       = var.data_governance_project_id[1]
-  confidential_data_project_id     = var.confidential_data_project_id[1]
-  non_confidential_data_project_id = var.non_confidential_data_project_id[1]
-  data_ingestion_project_id        = var.data_ingestion_project_id[1]
+  data_governance_project_id       = var.data_governance_project_id[0]
+  confidential_data_project_id     = var.confidential_data_project_id[0]
+  non_confidential_data_project_id = var.non_confidential_data_project_id[0]
+  data_ingestion_project_id        = var.data_ingestion_project_id[0]
   sdx_project_number               = var.sdx_project_number
   terraform_service_account        = var.terraform_service_account
   access_context_manager_policy_id = var.access_context_manager_policy_id
   delete_contents_on_destroy       = true
   perimeter_additional_members     = []
-  data_engineer_group              = var.group_email[1]
-  data_analyst_group               = var.group_email[1]
-  security_analyst_group           = var.group_email[1]
-  network_administrator_group      = var.group_email[1]
-  security_administrator_group     = var.group_email[1]
+  data_engineer_group              = var.group_email[0]
+  data_analyst_group               = var.group_email[0]
+  security_analyst_group           = var.group_email[0]
+  network_administrator_group      = var.group_email[0]
+  security_administrator_group     = var.group_email[0]
 }
