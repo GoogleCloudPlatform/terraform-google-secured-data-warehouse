fc78469c1d44d3eeb55ef32acaf73853467a59ba
Daniel da Silva Andrade <dandrade@ciandt.com>
2021-12-16 19:33:27 -0300

    fix semversion const and add check for basea attribution

--- test/integration/simple-example/controls/gcloud.rb
+++ test/integration/simple-example/controls/gcloud.rb
@@ -15,7 +15,8 @@
 project_id = attribute('project_id')
 blueprint_type = attribute('blueprint_type')
 blueprint_type_semversion = blueprint_type.split('/')[-1]
-SEMVERION_REGEX = /^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+blueprint_type_base = blueprint_type.split('/').slice(0, 3).join('/')
+SEMVERSION_REGEX = /^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
 
 boolean_policy_constraints = [
   'constraints/iam.disableServiceAccountCreation',
@@ -87,8 +88,14 @@ control 'gcloud' do
   end
 
   describe "check blueprint_type #{blueprint_type}" do
+    it 'should have a valid base attribution' do
+      expect("#{blueprint_type_base}" ).to eq 'blueprints/terraform/terraform-google-secured-data-warehouse'
+    end
+  end
+
+  describe "check blueprint_type #{blueprint_type}" do
     it 'should have a valid semversion' do
-      expect("#{blueprint_type_semversion}" ).to match SEMVERION_REGEX
+      expect("#{blueprint_type_semversion}" ).to match SEMVERSION_REGEX
     end
   end
 end
