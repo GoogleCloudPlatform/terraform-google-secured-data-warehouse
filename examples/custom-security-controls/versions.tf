f71f120a7beca0bbe9a50ede34f55b03c1599f23
Renato Rudnicki <renatojr@ciandt.com>
2022-03-07 11:59:55 -0300

    update versions

--- examples/custom-security-controls/versions.tf
+++ examples/custom-security-controls/versions.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,4 +16,33 @@
 
 terraform {
   required_version = ">= 0.13"
+
+  required_providers {
+
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.77"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.77"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.7"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.3"
+    }
+
+  }
 }
