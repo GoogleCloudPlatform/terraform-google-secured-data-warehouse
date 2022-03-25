73a0852eb7b2c52e4b80d9b49d528e7c707c5650
Renato Rudnicki <renatojr@ciandt.com>
2022-03-25 11:02:23 -0300

    Revert "update year to 2022"
    This reverts commit 36e83793452357d3c53ef2bead87119c0a4d9199.

--- helpers/sample-cc-generator/main.go
+++ helpers/sample-cc-generator/main.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import (
 
 const (
 	minIssueYear   = "2000"
-	maxIssueYear   = "2022"
+	maxIssueYear   = "2021"
 	minCreditLimit = 999
 	maxCreditLimit = 999999
 )
