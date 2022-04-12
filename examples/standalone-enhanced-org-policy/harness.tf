9b04680f601dc11a8daa5fec62f6e10589030d19
Renato Rudnicki <renatojr@ciandt.com>
2022-04-12 16:53:59 -0300

    Merging projects.tf into harness.tf

--- examples/standalone-enhanced-org-policy/harness.tf
+++ examples/standalone-enhanced-org-policy/harness.tf
@@ -14,6 +14,152 @@
  * limitations under the License.
  */
 
+locals {
+  projects_ids = {
+    data_ingestion   = var.data_ingestion_project_id,
+    governance       = var.data_governance_project_id,
+    non_confidential = var.non_confidential_data_project_id,
+    confidential     = var.confidential_data_project_id
+  }
+}
+
+resource "time_sleep" "wait_60_seconds_projects" {
+  create_duration = "60s"
+
+  depends_on = [
+    module.iam_projects
+  ]
+}
+
+resource "google_project_iam_binding" "remove_owner_role" {
+  for_each = local.projects_ids
+
+  project = each.value
+  role    = "roles/owner"
+  members = []
+
+  depends_on = [
+    time_sleep.wait_60_seconds_projects
+  ]
+}
+
+module "iam_projects" {
+  source = "../../test//setup/iam-projects"
+
+  data_ingestion_project_id        = var.data_ingestion_project_id
+  non_confidential_data_project_id = var.non_confidential_data_project_id
+  data_governance_project_id       = var.data_governance_project_id
+  confidential_data_project_id     = var.confidential_data_project_id
+  service_account_email            = var.terraform_service_account
+
+  depends_on = [
+    module.data_ingestion_project_id,
+    module.data_governance_project_id,
+    module.confidential_data_project_id,
+    module.non_confidential_data_project_id
+  ]
+}
+
+module "data_ingestion_project_id" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.0"
+
+  count = var.create_projects ? 1 : 0
+
+  name                    = var.data_ingestion_project_id
+  org_id                  = var.org_id
+  folder_id               = var.folder_id
+  billing_account         = var.billing_account
+  default_service_account = "deprivilege"
+
+  activate_apis = [
+    "cloudresourcemanager.googleapis.com",
+    "storage-api.googleapis.com",
+    "serviceusage.googleapis.com",
+    "iam.googleapis.com",
+    "pubsub.googleapis.com",
+    "bigquery.googleapis.com",
+    "accesscontextmanager.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudkms.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "artifactregistry.googleapis.com"
+  ]
+}
+
+module "data_governance_project_id" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.0"
+
+  count = var.create_projects ? 1 : 0
+
+  name                    = var.data_governance_project_id
+  org_id                  = var.org_id
+  folder_id               = var.folder_id
+  billing_account         = var.billing_account
+  default_service_account = "deprivilege"
+
+  activate_apis = [
+    "cloudresourcemanager.googleapis.com",
+    "storage-api.googleapis.com",
+    "serviceusage.googleapis.com",
+    "iam.googleapis.com",
+    "accesscontextmanager.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudkms.googleapis.com"
+  ]
+}
+
+module "confidential_data_project_id" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.0"
+
+  count = var.create_projects ? 1 : 0
+
+  name                    = var.confidential_data_project_id
+  org_id                  = var.org_id
+  folder_id               = var.folder_id
+  billing_account         = var.billing_account
+  default_service_account = "deprivilege"
+
+  activate_apis = [
+    "cloudresourcemanager.googleapis.com",
+    "storage-api.googleapis.com",
+    "serviceusage.googleapis.com",
+    "iam.googleapis.com",
+    "bigquery.googleapis.com",
+    "accesscontextmanager.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudkms.googleapis.com"
+  ]
+}
+
+module "non_confidential_data_project_id" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.0"
+
+  count = var.create_projects ? 1 : 0
+
+  name                    = var.non_confidential_data_project_id
+  org_id                  = var.org_id
+  folder_id               = var.folder_id
+  billing_account         = var.billing_account
+  default_service_account = "deprivilege"
+
+  activate_apis = [
+    "cloudresourcemanager.googleapis.com",
+    "storage-api.googleapis.com",
+    "serviceusage.googleapis.com",
+    "iam.googleapis.com",
+    "bigquery.googleapis.com",
+    "accesscontextmanager.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudkms.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "artifactregistry.googleapis.com"
+  ]
+}
+
 module "centralized_logging" {
   source                      = "../../modules/centralized-logging"
   projects_ids                = local.projects_ids
