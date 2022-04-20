ba36f8280ba6c27f9d0a1825174534c54dffcbc7
Renato Rudnicki <renatojr@ciandt.com>
2022-04-20 11:29:47 -0300

    adding outputs for example/standalone and test/setup

--- test/setup/outputs.tf
+++ test/setup/outputs.tf
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
+output "folder_id" {
+  value = var.folder_id
+}
+
 output "data_ingestion_project_id" {
   value = values(module.base_projects)[*].data_ingestion_project_id
 }
