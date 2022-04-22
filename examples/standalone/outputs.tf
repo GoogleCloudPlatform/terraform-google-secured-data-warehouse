844d02f11f52e9b87ac6c81e722b75a5137f38ff
andersonj-cit <83369108+andersonj-cit@users.noreply.github.com>
2022-04-22 11:42:30 -0300

    feat: Improve outputs for standalone example (#290)
    * add new outputs in output.tf file and add the outputs to the readme and list them by groups
    
    * fix lint issues
    
    * remove duplicate output and make description consistent
    
    * modifies suggested outputs for groups

--- examples/standalone/outputs.tf
+++ examples/standalone/outputs.tf
@@ -33,3 +33,158 @@ output "bigquery_non_confidential_table" {
     module.regional_deid_pipeline
   ]
 }
+
+output "blueprint_type" {
+  description = "Type of blueprint this module represents."
+  value       = module.secured_data_warehouse.blueprint_type
+}
+
+output "cmek_bigquery_crypto_key" {
+  description = "The Customer Managed Crypto Key for the BigQuery service."
+  value       = module.secured_data_warehouse.cmek_bigquery_crypto_key
+}
+
+output "cmek_confidential_bigquery_crypto_key" {
+  description = "The Customer Managed Crypto Key for the confidential BigQuery service."
+  value       = module.secured_data_warehouse.cmek_confidential_bigquery_crypto_key
+}
+
+output "cmek_data_ingestion_crypto_key" {
+  description = "The Customer Managed Crypto Key for the data ingestion crypto boundary."
+  value       = module.secured_data_warehouse.cmek_data_ingestion_crypto_key
+}
+
+output "cmek_reidentification_crypto_key" {
+  description = "The Customer Managed Crypto Key for the reidentification crypto boundary."
+  value       = module.secured_data_warehouse.cmek_reidentification_crypto_key
+}
+
+output "tek_wrapping_keyring" {
+  description = "The name of tek wrapping key"
+  value       = module.tek_wrapping_key.keyring
+}
+
+output "centralized_logging_bucket_name" {
+  description = "The name of the bucket created for storage logging."
+  value       = module.centralized_logging.bucket_name
+}
+
+output "confidential_data_dataflow_bucket_name" {
+  description = "The name of the bucket created for dataflow in the confidential data pipeline."
+  value       = module.secured_data_warehouse.confidential_data_dataflow_bucket_name
+}
+
+output "data_ingestion_dataflow_bucket_name" {
+  description = "The name of the bucket created for dataflow in the data ingestion pipeline."
+  value       = module.secured_data_warehouse.data_ingestion_dataflow_bucket_name
+}
+
+output "confidential_data_project_id" {
+  description = "The ID of the project created for confidential datasets and tables."
+  value       = module.base_projects.confidential_data_project_id
+
+}
+
+output "data_governance_project_id" {
+  description = "The ID of the project created for data governance."
+  value       = module.base_projects.data_governance_project_id
+
+}
+
+output "data_ingestion_project_id" {
+  description = "The ID of the project created for the data ingstion pipeline."
+  value       = module.base_projects.data_ingestion_project_id
+
+}
+
+output "non_confidential_data_project_id" {
+  description = "The id of the project created for non-confidential data."
+  value       = module.base_projects.non_confidential_data_project_id
+
+}
+
+output "template_project_id" {
+  description = "The id of the flex template created."
+  value       = module.template_project.project_id
+}
+
+output "data_ingestion_topic_name" {
+  description = "The topic created for data ingestion pipeline."
+  value       = module.secured_data_warehouse.data_ingestion_topic_name
+
+}
+
+output "pubsub_writer_service_account_email" {
+  description = "The PubSub writer service account email. Should be used to write data to the PubSub topics the data ingestion pipeline reads from."
+  value       = module.secured_data_warehouse.pubsub_writer_service_account_email
+}
+
+output "dataflow_controller_service_account_email" {
+  description = "The regional de identification pipeline service account."
+  value       = module.secured_data_warehouse.dataflow_controller_service_account_email
+}
+
+output "storage_writer_service_account_email" {
+  description = "The Storage writer service account email. Should be used to write data to the buckets the data ingestion pipeline reads from."
+  value       = module.secured_data_warehouse.storage_writer_service_account_email
+}
+
+output "confidential_data_perimeter_name" {
+  description = "Access context manager service perimeter name."
+  value       = module.secured_data_warehouse.confidential_service_perimeter_name
+}
+
+output "data_governance_perimeter_name" {
+  description = "Access context manager service perimeter name."
+  value       = module.secured_data_warehouse.data_governance_service_perimeter_name
+}
+
+output "data_ingestion_service_perimeter_name" {
+  description = "Access context manager service perimeter name."
+  value       = module.secured_data_warehouse.data_ingestion_service_perimeter_name
+}
+
+output "confidential_network_name" {
+  description = "The name of the confidential VPC being created."
+  value       = module.base_projects.confidential_network_name
+}
+
+output "confidential_network_self_link" {
+  description = "The URI of the confidential VPC being created."
+  value       = module.base_projects.confidential_network_self_link
+}
+
+output "confidential_subnets_self_link" {
+  description = "The self-links of confidential subnets being created."
+  value       = module.base_projects.confidential_subnets_self_link
+}
+
+output "data_ingestion_network_name" {
+  description = "The name of the data ingestion VPC being created."
+  value       = module.base_projects.data_ingestion_network_name
+}
+
+output "data_ingestion_network_self_link" {
+  description = "The URI of the data ingestion VPC being created."
+  value       = module.base_projects.data_ingestion_network_self_link
+}
+
+output "data_ingestion_subnets_self_link" {
+  description = "The self-links of data ingestion subnets being created."
+  value       = module.base_projects.data_ingestion_subnets_self_link
+}
+
+output "taxonomy_display_name" {
+  description = "The name of the taxonomy."
+  value       = google_data_catalog_taxonomy.secure_taxonomy.display_name
+}
+
+output "regional_reid_pipeline_job_id" {
+  description = "The unique ID of this job."
+  value       = module.regional_reid_pipeline.job_id
+}
+
+output "regional_deid_pipeline_job_id" {
+  description = "The unique ID of this job."
+  value       = module.regional_deid_pipeline.job_id
+}
