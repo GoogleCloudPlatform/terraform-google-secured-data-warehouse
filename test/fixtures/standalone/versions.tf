f140154eeae732afe86a086e9680878202e1581f
Renato Rudnicki <renatojr@ciandt.com>
2022-09-20 19:43:00 -0300

    rollback provider version

--- test/fixtures/standalone/versions.tf
+++ test/fixtures/standalone/versions.tf
@@ -21,12 +21,12 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 3.77"
     }
 
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.0"
+      version = "~> 3.77"
     }
 
     null = {
