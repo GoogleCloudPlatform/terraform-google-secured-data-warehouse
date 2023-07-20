42ce8a52cd5142f98fb0fa524b9821f3213487f0
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2023-07-20 15:45:40 -0300

    Comments third project verification

--- test/terraform-validator.sh
+++ test/terraform-validator.sh
@@ -38,15 +38,12 @@ data_ingestion_project_id_1=$(clean_string "${data_ingestion_projects[0]}")
 data_ingestion_project_number_1=$(get_project_number "${data_ingestion_project_id_1}")
 data_ingestion_project_id_2=$(clean_string "${data_ingestion_projects[1]}")
 data_ingestion_project_number_2=$(get_project_number "${data_ingestion_project_id_2}")
-data_ingestion_project_id_3=$(clean_string "${data_ingestion_projects[2]}")
-data_ingestion_project_number_3=$(get_project_number "${data_ingestion_project_id_3}")
 
 # gets the network self-links for confidential project created by setup which return an array
 # and get project number for each one
 IFS=',' read -ra data_ingestion_networks <<< "${TF_VAR_data_ingestion_network_self_link:?}"
 data_ingestion_network_1=$(clean_string "${data_ingestion_networks[0]}")
 data_ingestion_network_2=$(clean_string "${data_ingestion_networks[1]}")
-data_ingestion_network_3=$(clean_string "${data_ingestion_networks[2]}")
 
 # gets the project id for data governance created by setup which return an array
 # and get project number for each one
@@ -55,8 +52,6 @@ data_governance_project_id_1=$(clean_string "${data_governance_projects[0]}")
 data_governance_project_number_1=$(get_project_number "${data_governance_project_id_1}")
 data_governance_project_id_2=$(clean_string "${data_governance_projects[1]}")
 data_governance_project_number_2=$(get_project_number "${data_governance_project_id_2}")
-data_governance_project_id_3=$(clean_string "${data_governance_projects[2]}")
-data_governance_project_number_3=$(get_project_number "${data_governance_project_id_3}")
 
 # gets the project id for non-confidential data created by setup which return an array
 # and get project number for each one
@@ -65,8 +60,6 @@ non_confidential_data_project_id_1=$(clean_string "${non_confidential_data_proje
 non_confidential_data_project_number_1=$(get_project_number "${non_confidential_data_project_id_1}")
 non_confidential_data_project_id_2=$(clean_string "${non_confidential_data_projects[1]}")
 non_confidential_data_project_number_2=$(get_project_number "${non_confidential_data_project_id_2}")
-non_confidential_data_project_id_3=$(clean_string "${non_confidential_data_projects[2]}")
-non_confidential_data_project_number_3=$(get_project_number "${non_confidential_data_project_id_3}")
 
 # gets the project id for confidential data created by setup which return an array
 # and get project number for each one
@@ -75,14 +68,11 @@ confidential_project_id_1=$(clean_string "${confidential_projects[0]}")
 confidential_project_number_1=$(get_project_number "${confidential_project_id_1}")
 confidential_project_id_2=$(clean_string "${confidential_projects[1]}")
 confidential_project_number_2=$(get_project_number "${confidential_project_id_2}")
-confidential_project_id_3=$(clean_string "${confidential_projects[2]}")
-confidential_project_number_3=$(get_project_number "${confidential_project_id_3}")
 
 # gets the network self-link for confidential data project created by setup which return an array
 IFS=',' read -ra confidential_networks <<< "${TF_VAR_confidential_network_self_link:?}"
 confidential_network_1=$(clean_string "${confidential_networks[0]}")
 confidential_network_2=$(clean_string "${confidential_networks[1]}")
-confidential_network_3=$(clean_string "${confidential_networks[2]}")
 
 
 policy_file_path="$(pwd)/policy-library"
@@ -90,16 +80,16 @@ fixtures_path="test/fixtures"
 
 # replaces placeholders in policies files
 for f in "${policy_file_path}"/policies/constraints/*.yaml ; do
-    sed -i -e "s/NON_CONFIDENTIAL_DATA_PROJECT_ID_1/\"${non_confidential_data_project_id_1}\"/g" -e "s/NON_CONFIDENTIAL_DATA_PROJECT_ID_2/\"${non_confidential_data_project_id_2}\"/" -e "s/NON_CONFIDENTIAL_DATA_PROJECT_ID_3/\"${non_confidential_data_project_id_3}\"/" "${f}"
-    sed -i -e "s/NON_CONFIDENTIAL_DATA_PROJECT_NUMBER_1/\"${non_confidential_data_project_number_1}\"/g" -e "s/NON_CONFIDENTIAL_DATA_PROJECT_NUMBER_2/\"${non_confidential_data_project_number_2}\"/" -e "s/NON_CONFIDENTIAL_DATA_PROJECT_NUMBER_3/\"${non_confidential_data_project_number_3}\"/" "${f}"
-    sed -i -e "s/DATA_INGESTION_PROJECT_ID_1/\"${data_ingestion_project_id_1}\"/" -e "s/DATA_INGESTION_PROJECT_ID_2/\"${data_ingestion_project_id_2}\"/" -e "s/DATA_INGESTION_PROJECT_ID_3/\"${data_ingestion_project_id_3}\"/" "${f}"
-    sed -i -e "s/DATA_INGESTION_PROJECT_NUMBER_1/\"${data_ingestion_project_number_1}\"/" -e "s/DATA_INGESTION_PROJECT_NUMBER_2/\"${data_ingestion_project_number_2}\"/" -e "s/DATA_INGESTION_PROJECT_NUMBER_3/\"${data_ingestion_project_number_3}\"/" "${f}"
-    sed -i -e "s/DATA_GOVERNANCE_PROJECT_ID_1/\"${data_governance_project_id_1}\"/" -e "s/DATA_GOVERNANCE_PROJECT_ID_2/\"${data_governance_project_id_2}\"/" -e "s/DATA_GOVERNANCE_PROJECT_ID_3/\"${data_governance_project_id_3}\"/" "${f}"
-    sed -i -e "s/DATA_GOVERNANCE_PROJECT_NUMBER_1/\"${data_governance_project_number_1}\"/" -e "s/DATA_GOVERNANCE_PROJECT_NUMBER_2/\"${data_governance_project_number_2}\"/" -e "s/DATA_GOVERNANCE_PROJECT_NUMBER_3/\"${data_governance_project_number_3}\"/" "${f}"
-    sed -i -e "s/CONFIDENTIAL_PROJECT_ID_1/\"${confidential_project_id_1}\"/" -e "s/CONFIDENTIAL_PROJECT_ID_2/\"${confidential_project_id_2}/\"" -e "s/CONFIDENTIAL_PROJECT_ID_3/\"${confidential_project_id_3}\"/" "${f}"
-    sed -i -e "s/CONFIDENTIAL_PROJECT_NUMBER_1/\"${confidential_project_number_1}/" -e "s/CONFIDENTIAL_PROJECT_NUMBER_2/\"${confidential_project_number_2}\"/" -e "s/CONFIDENTIAL_PROJECT_NUMBER_3/\"${confidential_project_number_3}\"/" "${f}"
-    sed -i -e "s@DATA_INGESTION_NETWORK_1@\"${data_ingestion_network_1}\"@" -e "s@DATA_INGESTION_NETWORK_2@\"${data_ingestion_network_2}\"@" -e "s@DATA_INGESTION_NETWORK_3@\"${data_ingestion_network_3}\"@" "${f}"
-    sed -i -e "s@CONFIDENTIAL_NETWORK_1@\"${confidential_network_1}\"@" -e "s@CONFIDENTIAL_NETWORK_2@\"${confidential_network_2}\"@" -e "s@CONFIDENTIAL_NETWORK_3@\"${confidential_network_3}\"@" "${f}"
+    sed -i -e "s/NON_CONFIDENTIAL_DATA_PROJECT_ID_1/${non_confidential_data_project_id_1}/g" -e "s/NON_CONFIDENTIAL_DATA_PROJECT_ID_2/${non_confidential_data_project_id_2}/" "${f}"
+    sed -i -e "s/NON_CONFIDENTIAL_DATA_PROJECT_NUMBER_1/${non_confidential_data_project_number_1}/g" -e "s/NON_CONFIDENTIAL_DATA_PROJECT_NUMBER_2/${non_confidential_data_project_number_2}/" "${f}"
+    sed -i -e "s/DATA_INGESTION_PROJECT_ID_1/${data_ingestion_project_id_1}/" -e "s/DATA_INGESTION_PROJECT_ID_2/${data_ingestion_project_id_2}/" "${f}"
+    sed -i -e "s/DATA_INGESTION_PROJECT_NUMBER_1/${data_ingestion_project_number_1}/" -e "s/DATA_INGESTION_PROJECT_NUMBER_2/${data_ingestion_project_number_2}/" "${f}"
+    sed -i -e "s/DATA_GOVERNANCE_PROJECT_ID_1/${data_governance_project_id_1}/" -e "s/DATA_GOVERNANCE_PROJECT_ID_2/${data_governance_project_id_2}/" "${f}"
+    sed -i -e "s/DATA_GOVERNANCE_PROJECT_NUMBER_1/${data_governance_project_number_1}/" -e "s/DATA_GOVERNANCE_PROJECT_NUMBER_2/${data_governance_project_number_2}/" "${f}"
+    sed -i -e "s/CONFIDENTIAL_PROJECT_ID_1/${confidential_project_id_1}/" -e "s/CONFIDENTIAL_PROJECT_ID_2/${confidential_project_id_2}/" "${f}"
+    sed -i -e "s/CONFIDENTIAL_PROJECT_NUMBER_1/${confidential_project_number_1}/" -e "s/CONFIDENTIAL_PROJECT_NUMBER_2/${confidential_project_number_2}/" "${f}"
+    sed -i -e "s@DATA_INGESTION_NETWORK_1@${data_ingestion_network_1}@" -e "s@DATA_INGESTION_NETWORK_2@${data_ingestion_network_2}@" "${f}"
+    sed -i -e "s@CONFIDENTIAL_NETWORK_1@${confidential_network_1}@" -e "s@CONFIDENTIAL_NETWORK_2@${confidential_network_2}@" "${f}"
 done
 
 # calls docker image script to run terraform validator
